6e8dc1d3425cfb13756556f844c68dd3d7bcf060
Yizack Rangel <yizackr@gmail.com>
2023-06-12 17:57:52 +0200

    Initial commit

---
+++ nuxt-config.js
@@ -0,0 +1,23 @@
+export default {
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "",
+      htmlAttrs: {
+        lang: "en"
+      },
+      bodyAttrs: {
+        "data-bs-theme": "dark"
+      },
+      meta: [
+        { name: "robots", content: "index, follow" },
+      ]
+    }
+  },
+  css: [
+    "bootstrap/dist/css/bootstrap.min.css"
+  ],
+  modules: [],
+  runtimeConfig: {},
+};

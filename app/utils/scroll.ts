53494a17517d481a6aec10547df432a9c1d978aa
Yizack Rangel <yizackr@gmail.com>
2024-06-23 18:25:57 -0500

    refactor: nuxt v4 compatibility

--- app/utils/scroll.ts
+++ app/utils/scroll.ts
@@ -1,7 +1,7 @@
 export const setScrollBehavior = () => {
   const { options } = useRouter();
   options.scrollBehavior = (to) => {
-    if (to.hash === "") return { left: 0, top: 0 };
+    if (!to.hash) return { left: 0, top: 0 };
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve({ el: to.hash, top: 55, left: 0, behavior: "smooth" });

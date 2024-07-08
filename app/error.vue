23a25a696daff83c985936b2ab553e4bac2ccf76
Yizack Rangel <yizackr@gmail.com>
2024-07-08 05:33:04 -0500

    chore: improvements

--- app/error.vue
+++ app/error.vue
@@ -1,12 +1,16 @@
-<script setup>
-const { statusCode, statusMessage, message } = useError().value;
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({
+  error: { type: Object as () => NuxtError, required: true }
+});
 </script>
 
 <template>
   <div class="container vh-100 d-flex justify-content-center align-items-center">
     <div class="bg-body p-5 rounded-3 shadow">
-      <h1>Error <span class="text-primary">{{ statusCode }}</span></h1>
-      <h5>{{ "error_occurred" }}: {{ message || statusMessage }}</h5>
+      <h1>Error <span class="text-primary">{{ error.statusCode }}</span></h1>
+      <h5>{{ "error_occurred" }}: {{ error.statusMessage || error.message }}</h5>
       <p>{{ "go_back" }}: <NuxtLink to="/" class="text-decoration-underline">link</NuxtLink></p>
     </div>
   </div>

6cb1c78d540b5345e768006c423d52e36d609bf9
Yizack Rangel <yizackr@gmail.com>
2025-05-05 04:58:19 -0500

    refactor: move composables

--- app/composables/modal.ts
+++ app/composables/modal.ts
@@ -1,24 +1,3 @@
-export const useFormState = <T extends Record<string, unknown>>(initialState: T) => {
-  const data = ref<T>({ ...initialState });
-  const methods = {
-    /**
-     * Reset all fields or specific fields
-     * @param fields
-     */
-    reset (...fields: (keyof T)[]) {
-      if (!fields.length) {
-        data.value = { ...initialState };
-        return;
-      }
-      for (const field of fields) {
-        data.value[field] = initialState[field];
-      }
-    }
-  };
-  Object.assign(data, methods);
-  return data as Ref<T> & typeof methods;
-};
-
 export const useModal = (id: string) => {
   const { $bootstrap } = useNuxtApp();
   return ref({

f7c93185b2c6018fe4fff6b74ec16cc7cfdde3db
Yizack Rangel <yizackr@gmail.com>
2025-05-05 04:58:04 -0500

    fix: error message

--- app/error.vue
+++ app/error.vue
@@ -10,7 +10,7 @@ defineProps<{
   <div class="container vh-100 d-flex justify-content-center align-items-center">
     <div class="bg-body p-5 rounded-3 shadow">
       <h1>Error <span class="text-primary">{{ error.statusCode }}</span></h1>
-      <h5>{{ "error_occurred" }}: {{ error.statusMessage || error.message }}</h5>
+      <h5>{{ "error_occurred" }}: {{ error.message || error.statusMessage }}</h5>
       <p>{{ "go_back" }}: <NuxtLink to="/" class="text-decoration-underline">link</NuxtLink></p>
     </div>
   </div>
